9a15810c1d3be1d4e542d4003e323d71a767c41d
agiledots <agiledots@gmail.com>
2018-12-27 16:27:32 +0900

    Create initUser.js

---
+++ initUser.js
@@ -0,0 +1,3 @@
+db.user.insertOne({"username": "u001", "password": "test1"});
+db.user.insertOne({"username": "u002", "password": "test2"});
+db.user.insertOne({"username": "u003", "password": "test3"});
